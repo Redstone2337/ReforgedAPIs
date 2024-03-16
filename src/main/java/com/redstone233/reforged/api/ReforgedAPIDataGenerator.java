8cf395ce9391bfb4d28ad31aec412bf4256ad159
Redstone2337200 <100214029+Redstone2337200@users.noreply.github.com>
2024-03-16 21:44:13 +0800

    Create ReforgedAPIDataGenerator.java

---
+++ src/main/java/com/redstone233/reforged/api/ReforgedAPIDataGenerator.java
@@ -0,0 +1,11 @@
+package com.redstone233.reforged.api;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ReforgedAPIDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
